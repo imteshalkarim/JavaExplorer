98772b8a78e60eac9b4b5274a928cd7fc6fdbf9f
Mohammed Imteshal Karim <imteshal.mik@gmail.com>
2021-09-14 22:35:59 +0530

    Create reverse_number.java
    A program in JAVA to enter a number and reverse it along with checking if the number is a palindrome or not.

---
+++ reverse_number.java
@@ -0,0 +1,23 @@
+/* A program in JAVA to enter a number and reverse it along with checking if the number is a palindrome or not. */
+import java.util.*;
+public class reverse_number
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        int a=n,rn=0;
+        while(a>0)
+        {
+            int dg=a%10;
+            rn=rn*10+dg;
+            a=a/10;
+        }    
+        if(rn==n)
+        System.out.println("The number is a palindrome:    "+rn);
+        else
+        System.out.println("The number when reversed =    "+rn);
+        sc.close();
+    }    
+}
