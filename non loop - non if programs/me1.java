40b48b1087df3c97f4356f0293d0a18e958d0302
Mohammed Imteshal Karim <imteshal.mik@gmail.com>
2023-03-30 14:03:53 +0530

    Update me1.java

--- non loop - non if programs/me1.java
+++ non loop - non if programs/me1.java
@@ -1,1 +1,12 @@
+//WAP in java to display name, school and class in different lines
+public class me1 {
+  public static void main(String[] args) {
+    String name = "atomicswas";
+    String school = "Xavier's Institute";
+    String className = "Class 10";
 
+    System.out.println("Name: " + name);
+    System.out.println("School: " + school);
+    System.out.println("Class: " + className);
+  }
+}
