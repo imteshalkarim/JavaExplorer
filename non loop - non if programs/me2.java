f0815593cefe39f34459f1c640d2fe96d81b3657
Mohammed Imteshal Karim <imteshal.mik@gmail.com>
2023-03-30 14:16:13 +0530

    Create me2.java

---
+++ non loop - non if programs/me2.java
@@ -0,0 +1,12 @@
+//Display your name, age and address in one line but with different print functions
+public class me2 {
+    public static void main(String[] args) {
+      String name = "atomicswas";
+      int age = 30;
+      String address = "123 Main Street, Anytown, USA";
+  
+      System.out.print("Name: " + name + " ");
+      System.out.print("Age: " + age + " ");
+      System.out.println("Address: " + address);
+    }
+  }
